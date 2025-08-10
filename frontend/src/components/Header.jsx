76def0fe74d317bdd3c570eb2a3bdbd9012b1a46
Williamarvin <williamarvin111@gmail.com>
2025-08-10 23:21:42 +0800

    enlarge title in heading

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import EditNoteIcon from '@mui/icons-material/EditNote';
 function Header(){
     return (
         <div id='header'>
-            <h1 id="title"><EditNoteIcon style={{fontSize:"large"}}/> William's Personal Todo App</h1>
+            <h1 id="title"><EditNoteIcon style={{fontSize:"xx-large"}}/> William's Personal Todo App</h1>
         </div>
     )
 }
--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,7 +1,6 @@
 function Note(prop){
     return (
         <div>
-            <h1>test</h1>
             <h2>{prop.listNotes}</h2>
         </div>
     )
